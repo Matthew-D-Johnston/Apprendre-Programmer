b047630167b33a7c6c28b9373b3a5db6f5937fb5
Matthew D. Johnston <mattdjohnston92@gmail.com>
2019-05-15 21:51:33 -0400

    Ajouter des dossiers d'exercices de chapitre 5

---
+++ justification.rb
@@ -0,0 +1,10 @@
+# justification.rb
+
+largeurLigne = 40
+
+puts ("Table des matières".ljust(largeurLigne))
+puts " "
+puts ("Chapitre 1 : Nombres  ".ljust(largeurLigne/2) + "page 1".rjust(largeurLigne/2))
+puts ("Chapitre 2 : Lettres  ".ljust(largeurLigne/2) + "page 72".rjust(largeurLigne/2))
+puts ("Chapitre 3 : Variables".ljust(largeurLigne/2) + "page 118".rjust(largeurLigne/2))
+
---
+++ répeter.rb
@@ -0,0 +1,7 @@
+# répeter.rb
+
+puts "Qu'est-ce que vous voulez ?"
+réponse = gets.chomp
+
+puts "Mais qu'est-ce ça veux dire '#{réponse}'?!?"
+
